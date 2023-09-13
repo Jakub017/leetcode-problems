5a07252049327e40a284fbd219030e9b92426e47
Jakub Lipiński <84547080+Jakub017@users.noreply.github.com>
2023-09-13 13:53:38 +0200

    Time: 5 ms (67.65%), Space: 19.4 MB (9.88%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.php
@@ -0,0 +1,21 @@
+class Solution {
+
+    /**
+     * @param String $s
+     * @return Integer
+     */
+    function lengthOfLastWord($s) {
+       $count = 0;
+       for($i = strlen($s) -1 ; $i >= 0; $i--) {
+           if($s[$i] != " ") {
+               $count++;
+           } else {
+               if($count > 0) {
+                   break;
+               }
+           }
+        }
+
+        return $count;
+    }
+}
